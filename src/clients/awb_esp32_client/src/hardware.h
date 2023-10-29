111800a8830fbacfd8ec95771e1af6ee7521795e
Daniel Springwald <daniel@springwald.de>
2023-10-30 00:27:11 +0100

    Update hardware.h

--- src/clients/awb_esp32_client/src/hardware.h
+++ src/clients/awb_esp32_client/src/hardware.h
@@ -12,7 +12,7 @@
 
 /* autoplay state selector */
 // if a servo position feedback is used as a state selector, define the servo channel here.
-// if you don't use a servo as state selector, set this to -1
+// if you don't use a servo as state selector, set this to -1 or undefine it
 // #define AUTOPLAY_STATE_SELECTOR_STS_SERVO_CHANNEL 9
 // if the servo position feedback is not exatly 0 at the first state, define the offset here (-4096 to 4096)
 #define AUTOPLAY_STATE_SELECTOR_STS_SERVO_POS_OFFSET 457
@@ -21,8 +21,8 @@
 // #define USE_STS_SERVO
 #define STS_SERVO_SPEED 1500         // speed, default = 1500
 #define STS_SERVO_ACC 10             // acceleration, default = 100
-#define STS_SERVO_RXD 18             // GPIO 18
-#define STS_SERVO_TXD 19             // GPIO 19
+#define STS_SERVO_RXD 18             // eg. GPIO 18 for wavesahre servo driver
+#define STS_SERVO_TXD 19             // eg. GPIO 19 for wavesahre servo driver
 #define STS_SERVO_MAX_TEMPERATURE 45 // max temperature (celsius) before servo is disabled
 #define STS_SERVO_MAX_LOAD 250       // max load before servo is disabled
 
