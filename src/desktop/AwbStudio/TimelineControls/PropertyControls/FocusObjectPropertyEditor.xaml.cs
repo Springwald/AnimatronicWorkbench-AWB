b38be0f87cc2f888a408117caa4c091b2cecafe9
Daniel Springwald <daniel@springwald.de>
2024-04-11 23:10:50 +0200

    Introducing property editor for servo point

--- src/desktop/AwbStudio/TimelineControls/PropertyControls/FocusObjectPropertyEditor.xaml.cs
+++ src/desktop/AwbStudio/TimelineControls/PropertyControls/FocusObjectPropertyEditor.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Animatronic WorkBench
+// https://github.com/Springwald/AnimatronicWorkBench-AWB
+//
+// (C) 2024 Daniel Springwald  - 44789 Bochum, Germany
+// https://daniel.springwald.de - daniel@springwald.de
+// All rights reserved   -  Licensed under MIT License
+
+using Awb.Core.Actuators;
+using Awb.Core.ActuatorsAndObjects;
+using Awb.Core.Clients;
+using Awb.Core.Player;
+using AwbStudio.TimelineEditing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AwbStudio.TimelineControls.PropertyControls
 {
@@ -20,9 +20,77 @@ namespace AwbStudio.TimelineControls.PropertyControls
     /// </summary>
     public partial class FocusObjectPropertyEditor : UserControl
     {
+        private TimelineViewContext? _viewContext;
+        private PlayPosSynchronizer _playPosSynchronizer;
+        private IAwbObject? _focusObject;
+
         public FocusObjectPropertyEditor()
         {
             InitializeComponent();
+            Unloaded += FocusObjectPropertyEditor_Unloaded;
+        }
+
+        private void FocusObjectPropertyEditor_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_viewContext != null)
+                _viewContext.Changed -= ViewContext_Changed;
+
+            if (_playPosSynchronizer != null)
+                _playPosSynchronizer.OnPlayPosChanged -= PlayPosSynchronizer_OnPlayPosChanged;
+        }
+
+        public void Init(TimelineViewContext timelineViewContext, PlayPosSynchronizer playPosSynchronizer)
+        {
+            _viewContext = timelineViewContext;
+            _viewContext.Changed += ViewContext_Changed;
+            _playPosSynchronizer = playPosSynchronizer;
+            _playPosSynchronizer.OnPlayPosChanged += PlayPosSynchronizer_OnPlayPosChanged;
+        }
+
+        private void PlayPosSynchronizer_OnPlayPosChanged(object? sender, int e)
+        {
+            if (_focusObject is IServo servo)
+            {
+
+            };
+        }
+
+        private void ViewContext_Changed(object? sender, ViewContextChangedEventArgs e)
+        {
+            switch (e.ChangeType)
+            {
+                case ViewContextChangedEventArgs.ChangeTypes.FocusObject:
+                    if (_viewContext != null)
+                    {
+                        _focusObject = _viewContext.ActualFocusObject;
+                        if (_focusObject == null)
+                        {
+                            LabelPropertyEditorTitle.Content = "Properties [nothing selected]";
+                            this.PropertyEditorGrid.Children.Clear();
+                            return;
+                        }
+
+                        LabelPropertyEditorTitle.Content = $"Properties for {_focusObject.Title}";
+
+                        // cast the focus object to a property editor
+                        if (_focusObject is IServo servo)
+                        {
+                            var propertyEditor = new ServoPropertiesControl();
+                            this.PropertyEditorGrid.Children.Clear();
+                            this.PropertyEditorGrid.Children.Add(propertyEditor);
+                        }
+                    }
+                    break;
+
+                case ViewContextChangedEventArgs.ChangeTypes.Duration:
+                case ViewContextChangedEventArgs.ChangeTypes.BankIndex:
+                case ViewContextChangedEventArgs.ChangeTypes.PixelPerMs:
+                case ViewContextChangedEventArgs.ChangeTypes.Scroll:
+                    break;
+
+                default:
+                    throw new System.ArgumentOutOfRangeException($"{nameof(e.ChangeType)}:{e.ChangeType}");
+            }
         }
     }
 }
