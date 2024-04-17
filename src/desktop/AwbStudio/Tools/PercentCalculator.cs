09e9a10ceae0c1a6908e98b6f2020cadd07b5f62
Daniel Springwald <daniel@springwald.de>
2024-04-17 16:27:07 +0200

    Create PercentCalculator.cs

---
+++ src/desktop/AwbStudio/Tools/PercentCalculator.cs
@@ -0,0 +1,39 @@
+﻿// Animatronic WorkBench
+// https://github.com/Springwald/AnimatronicWorkBench-AWB
+//
+// (C) 2024 Daniel Springwald  - 44789 Bochum, Germany
+// https://daniel.springwald.de - daniel@springwald.de
+// All rights reserved   -  Licensed under MIT License
+
+namespace AwbStudio.Tools
+{
+    internal class PercentCalculator
+    {
+        public PercentCalculator(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public double Min { get; }
+        public double Max { get; }
+
+        public double CalculatePercent(double value)
+        {
+            if (Min > Max)
+            {
+                return 100 - ((value - Max) / (Min - Max) * 100);
+            }
+            return (value - Min) / (Max - Min) * 100;
+        }
+
+        public double CalculateValue(double percent)
+        {
+            if (Min > Max)
+            {
+                return Max + (100 - percent) / 100 * (Min - Max);
+            }
+            return Min + percent / 100 * (Max - Min);
+        }
+    }
+}
