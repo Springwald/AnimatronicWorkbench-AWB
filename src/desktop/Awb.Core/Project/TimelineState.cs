1275d949ba231fc8451ba6b3c9e27e74d659ac65
Daniel Springwald <daniel@springwald.de>
2024-04-24 19:06:26 +0200

    Update TimelineState.cs

--- src/desktop/Awb.Core/Project/TimelineState.cs
+++ src/desktop/Awb.Core/Project/TimelineState.cs
@@ -12,7 +12,16 @@ namespace Awb.Core.Project
         public int Id { get; internal set; }
         public string Title { get; internal set; }
 
+        /// <summary>
+        /// Export timelines with this state to the client project souurce code
+        /// </summary>
         public bool Export { get; internal set; } = true;
+
+        /// <summary>
+        /// Play the timeline automatically when the state is active.
+        /// If false: The timeline will be played when the user activates it manually by remote control
+        /// </summary>
+        public bool AutoPlay { get; internal set; } = true;
 
         /// <summary>
         /// The state is only available when one of this inputs are on
