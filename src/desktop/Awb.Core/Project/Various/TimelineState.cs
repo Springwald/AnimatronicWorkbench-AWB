778ff215fbb84b71a6ae268f66ceb87efadaa409
Daniel Springwald <daniel@springwald.de>
2024-06-11 15:43:26 +0200

    Made Input IDs editable

--- src/desktop/Awb.Core/Project/Various/TimelineState.cs
+++ src/desktop/Awb.Core/Project/Various/TimelineState.cs
@@ -6,32 +6,50 @@
 // All rights reserved   -  Licensed under MIT License
 
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Awb.Core.Project.Various
 {
     public class TimelineState : IProjectObjectListable
     {
-        public required int Id { get;  set; }
+        public required int Id { get; set; }
 
         [DisplayName("Title")]
-        public required string Title { get;  set; }
+        public required string Title { get; set; }
 
         [DisplayName("Export")]
         [Description("Export timelines with this state to the client project souurce code")]
-        public bool Export { get;  set; } = true;
+        public bool Export { get; set; } = true;
 
         [DisplayName("Autoplay")]
         [Description("Play the timeline automatically when the state is active.\r\nIf false: The timeline will be played when the user activates it manually by remote control")]
-        public bool AutoPlay { get;  set; } = true;
+        public bool AutoPlay { get; set; } = true;
+
+        public int[] PositiveInputs { get; set; } = Array.Empty<int>();
+        public int[] NegativeInputs { get; set; } = Array.Empty<int>();
+
 
         [DisplayName("Positive inputs IDs")]
-        [Description("The state is only available when one of this inputs is on")]
-        public int[] PositiveInputs { get;  set; } = Array.Empty<int>();
+        [Description("The state is only available when one of this inputs is on.\r\nFormat: Use commas to list multiple IDs.")]
+        [RegularExpression(@"\d+(,\d+)*", ErrorMessage = "Please enter a comma separated list of input IDs.")]
+        [JsonIgnore]
+        public string PositiveInputsAsString
+        {
+            get => StringFromIntArray(PositiveInputs);
+            set => PositiveInputs = IntArrayFromString(value);
+        }
 
         [DisplayName("Negative inputs IDs")]
-        [Description("The state is NOT available when one of this inputs is on")]
-        public int[] NegativeInputs { get;  set; } = Array.Empty<int>();
+        [Description("The state is NOT available when one of this inputs is on.\r\nFormat: Use commas to list multiple IDs.")]
+        [RegularExpression(@"\d+(,\d+)*", ErrorMessage = "Please enter a comma separated list of input IDs.")]
+        [JsonIgnore]
+        public string NegativeInputsAsString
+        {
+            get => StringFromIntArray(NegativeInputs);
+            set => NegativeInputs = IntArrayFromString(value);
+        }
+
 
         public IEnumerable<ProjectProblem> GetProblems(AwbProject project)
         {
@@ -44,6 +62,16 @@ namespace Awb.Core.Project.Various
         [JsonIgnore]
         public string TitleDetailled => $"TimelineState {TitleShort}";
 
+        private string StringFromIntArray(int[] value)
+            => string.Join(",", value.Select(x => x.ToString()));
 
+        private int[] IntArrayFromString(string positiveInputsAsString)
+        {
+            if (string.IsNullOrWhiteSpace(positiveInputsAsString))
+            {
+                return Array.Empty<int>();
+            }
+            return positiveInputsAsString.Split(',').Select(x => int.Parse(x)).ToArray();
+        }
     }
 }
