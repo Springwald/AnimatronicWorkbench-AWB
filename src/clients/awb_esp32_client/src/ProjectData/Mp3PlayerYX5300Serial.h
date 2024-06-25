9392d66f4633897e9b8244e754374eb8abdad2f8
Daniel Springwald <daniel@springwald.de>
2024-06-26 00:37:13 +0200

    Update Mp3PlayerYX5300Serial.h

--- src/clients/awb_esp32_client/src/ProjectData/Mp3PlayerYX5300Serial.h
+++ src/clients/awb_esp32_client/src/ProjectData/Mp3PlayerYX5300Serial.h
@@ -16,12 +16,11 @@ private:
     MD_YX5300 _mp3;
 
 public:
-    String id;
     String name;
     TCallBackErrorOccured _errorOccured;
 
     // the constructor
-    Mp3PlayerYX5300Serial(int rxPin, int txPin, String id, String name) : _mp3Stream(rxPin, txPin), _mp3(MD_YX5300(_mp3Stream)), id(id), name(name)
+    Mp3PlayerYX5300Serial(int rxPin, int txPin, String name) : _mp3Stream(rxPin, txPin), _mp3(MD_YX5300(_mp3Stream)), name(name)
     {
         _mp3Stream.begin(MD_YX5300::SERIAL_BPS);
         _mp3.begin();
