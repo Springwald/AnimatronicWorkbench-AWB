770d55b4818ef38eb36a11b63e203cbc430a9514
Daniel Springwald <daniel@springwald.de>
2024-06-26 00:25:56 +0200

    Update Mp3PlayerYX5300Serial.h

--- src/clients/awb_esp32_client/src/ProjectData/Mp3PlayerYX5300Serial.h
+++ src/clients/awb_esp32_client/src/ProjectData/Mp3PlayerYX5300Serial.h
@@ -4,10 +4,8 @@
 #include <Arduino.h>
 #include <vector>
 #include <MD_YX5300.h>
+#include <string>
 #include <SoftwareSerial.h>
-
-// SoftwareSerial MP3Stream(13, 14);
-//  MD_YX5300 mp3(MP3Stream);
 
 class Mp3PlayerYX5300Serial
 {
@@ -20,16 +18,12 @@ private:
     SoftwareSerial _mp3Stream; // MP3 player serial stream for comms
     MD_YX5300 _mp3;
 
-    // MP3Stream Serial3;
-    //  MP3Stream Serial3(13, 14);
-    // MD_YX5300 _mp3(MP3Stream);
-    // MD_YX5300 mp3(MP3Stream);
-    //  SoftwareSerial _mp3Stream; // MP3 player serial stream for comms
-    //  MD_YX5300 _mp3;
-
 public:
+    String id;
+    String name;
+
     // the constructor
-    Mp3PlayerYX5300Serial(int rxPin, int txPin, TCallBackErrorOccured errorOccured, TCallBackMessageToShow messageToShow) : _errorOccured(errorOccured), _messageToShow(messageToShow), _mp3Stream(rxPin, txPin), _mp3(MD_YX5300(_mp3Stream))
+    Mp3PlayerYX5300Serial(int rxPin, int txPin, String id, String name) : _mp3Stream(rxPin, txPin), _mp3(MD_YX5300(_mp3Stream)), id(id), name(name)
     {
         _mp3Stream.begin(MD_YX5300::SERIAL_BPS);
         _mp3.begin();
