327b1b37d517f3dfa8ecece6f63e904f02723ddf
Daniel Springwald <daniel@springwald.de>
2024-06-26 00:22:37 +0200

    Added Mp3Player

---
+++ src/clients/awb_esp32_client/src/ProjectData/Mp3PlayerYX5300Serial.h
@@ -0,0 +1,44 @@
+#ifndef Mp3PlayerYX5300Serial_manager_h
+#define Mp3PlayerYX5300Serial_manager_h
+
+#include <Arduino.h>
+#include <vector>
+#include <MD_YX5300.h>
+#include <SoftwareSerial.h>
+
+// SoftwareSerial MP3Stream(13, 14);
+//  MD_YX5300 mp3(MP3Stream);
+
+class Mp3PlayerYX5300Serial
+{
+    using TCallBackErrorOccured = std::function<void(String)>;
+    using TCallBackMessageToShow = std::function<void(String)>;
+
+private:
+    TCallBackErrorOccured _errorOccured;
+    TCallBackMessageToShow _messageToShow;
+    SoftwareSerial _mp3Stream; // MP3 player serial stream for comms
+    MD_YX5300 _mp3;
+
+    // MP3Stream Serial3;
+    //  MP3Stream Serial3(13, 14);
+    // MD_YX5300 _mp3(MP3Stream);
+    // MD_YX5300 mp3(MP3Stream);
+    //  SoftwareSerial _mp3Stream; // MP3 player serial stream for comms
+    //  MD_YX5300 _mp3;
+
+public:
+    // the constructor
+    Mp3PlayerYX5300Serial(int rxPin, int txPin, TCallBackErrorOccured errorOccured, TCallBackMessageToShow messageToShow) : _errorOccured(errorOccured), _messageToShow(messageToShow), _mp3Stream(rxPin, txPin), _mp3(MD_YX5300(_mp3Stream))
+    {
+        _mp3Stream.begin(MD_YX5300::SERIAL_BPS);
+        _mp3.begin();
+        _mp3.setSynchronous(false);
+        _mp3.check(); // run the mp3 receiver
+    }
+
+    bool playSound(int trackNo);
+    bool stopSound();
+};
+
+#endif
