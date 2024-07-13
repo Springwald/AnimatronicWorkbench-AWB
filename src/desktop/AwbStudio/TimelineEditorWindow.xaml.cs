94401b4ff6dbfc8695c22f1561b79adaa90b5fa4
Daniel Springwald <daniel@springwald.de>
2024-07-13 14:45:08 +0200

    improved selection sorting and titles

--- src/desktop/AwbStudio/TimelineEditorWindow.xaml.cs
+++ src/desktop/AwbStudio/TimelineEditorWindow.xaml.cs
@@ -136,7 +136,7 @@ namespace AwbStudio
             }
 
             // fill timeline state chooser
-            ComboTimelineStates.ItemsSource = _project.TimelinesStates?.Select(ts => GetTimelineStateName(ts)).ToList();
+            ComboTimelineStates.ItemsSource = _project.TimelinesStates?.Select(ts => $"[{ts.Id}] {GetTimelineStateName(ts)}").ToList();
             TimelineChooser.ProjectTitle = _project.ProjectMetaData.ProjectTitle;
             TimelineChooser.FileManager = _timelineDataService;
 
