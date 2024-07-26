b967f6d090e1bac55b27b5e2ac6c0c87318daf79
Daniel Springwald <daniel@springwald.de>
2024-07-26 17:42:48 +0200

    Update CustomCode.h

--- src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.h
+++ src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.h
@@ -14,18 +14,24 @@ class CustomCode
 {
 protected:
     NeopixelManager *neopixelManager;
-    void setButtonLightByTouch(uint16_t btnIndex, uint8_t touchPin);
 
     /* cc-start-protected - insert your protected code here before the end-protected comment: */
+
+    void setButtonLightByTouch(uint16_t btnIndex, uint8_t touchPin);
+
     /* cc-end-protected  */
 
 public:
     CustomCode(NeopixelManager *neopixelManager) : neopixelManager(neopixelManager)
     {
+        /* cc-start-constructor - insert your protected code here before the end-constructor comment: */
+        /* cc-end-constructor  */
     }
 
     ~CustomCode()
     {
+        /* cc-start-destrucutor - insert your protected code here before the end-destrucutor comment: */
+        /* cc-end-destrucutor  */
     }
 
     void setup();
