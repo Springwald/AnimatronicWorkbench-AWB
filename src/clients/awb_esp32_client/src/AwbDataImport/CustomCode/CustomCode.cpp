70288796ccc61c0939a42593effc63b2ec42d68e
Daniel Springwald <daniel@springwald.de>
2024-07-26 16:37:49 +0200

    added structures for custom code templating

--- src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.cpp
+++ src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.cpp
@@ -5,18 +5,22 @@
 
 void CustomCode::setup()
 {
+    /* cc-start-setup - insert your setup code here before the end-setup comment: */
+    /* cc-end-setup  */
 }
 
 void CustomCode::loop()
 {
+    /* cc-start-loop - insert your loop code here before the end-loop comment: */
+
     /* neopixelManager->setSingleLED(0, 128, 0, 0);
-     neopixelManager->setSingleLED(1, 128, 0, 0);
-     neopixelManager->setSingleLED(2, 128, 128, 0);
-     neopixelManager->setSingleLED(3, 128, 128, 0);
-     neopixelManager->setSingleLED(4, 0, 128, 0);
-     neopixelManager->setSingleLED(5, 0, 128, 0);
-     neopixelManager->setSingleLED(6, 0, 0, 128);
-     neopixelManager->setSingleLED(7, 0, 0, 128);*/
+    neopixelManager->setSingleLED(1, 128, 0, 0);
+    neopixelManager->setSingleLED(2, 128, 128, 0);
+    neopixelManager->setSingleLED(3, 128, 128, 0);
+    neopixelManager->setSingleLED(4, 0, 128, 0);
+    neopixelManager->setSingleLED(5, 0, 128, 0);
+    neopixelManager->setSingleLED(6, 0, 0, 128);
+    neopixelManager->setSingleLED(7, 0, 0, 128);*/
 
     // working touch pins on ESP-32-WROOM: 4, 12, 13, 14, 27, 32, 33
 
@@ -24,7 +28,11 @@ void CustomCode::loop()
     setButtonLightByTouch(1, 33);
     setButtonLightByTouch(2, 27);
     setButtonLightByTouch(3, 14);
+
+    /* cc-end-loop  */
 }
+
+/* cc-start-functions - insert your functions here before the end-functions comment: */
 
 void CustomCode::setButtonLightByTouch(uint16_t btnIndex, uint8_t touchPin)
 {
@@ -40,3 +48,5 @@ void CustomCode::setButtonLightByTouch(uint16_t btnIndex, uint8_t touchPin)
         neopixelManager->setSingleLED(btnIndex * 2 + 1, 0, 0, 128);
     }
 }
+
+/* cc-end-functions  */
--- src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.h
+++ src/clients/awb_esp32_client/src/AwbDataImport/CustomCode/CustomCode.h
@@ -5,11 +5,19 @@
 #include <String.h>
 #include <Actuators/NeopixelManager.h>
 
+/*
+    Enter your custom code in this header file and the corresponding cpp file.
+    Only write code beween the cc-start and cc-end comments, otherwise it will be overwritten and lost.
+*/
+
 class CustomCode
 {
 protected:
     NeopixelManager *neopixelManager;
     void setButtonLightByTouch(uint16_t btnIndex, uint8_t touchPin);
+
+    /* cc-start-protected - insert your protected code here before the end-protected comment: */
+    /* cc-end-protected  */
 
 public:
     CustomCode(NeopixelManager *neopixelManager) : neopixelManager(neopixelManager)
